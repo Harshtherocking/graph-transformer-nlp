e74e473c60203ef2cd0640479461e617cf4fc5e4
Harsh <harsh.thunder.happy@gmail.com>
2025-02-01 21:34:59 +0530

    arch updated

--- model/arch.py
+++ model/arch.py
@@ -2,11 +2,26 @@ import os
 import torch 
 import torch.nn as nn 
 import torch.nn.functional as F
-import torchtune
+from torchtune.modules.position_embeddings import RotaryPositionalEmbeddings
 
-embed_path = os.path.join(os.getcwd(), "utils", "embed.pt")
+import os
+import sys
+from dotenv import load_dotenv
+
+load_dotenv()
+
+project_root = os.getenv('PROJECT_ROOT')
+os.chdir(project_root)
+sys.path.append(project_root)
+
+MAX_SEQ_LEN = int(os.getenv('MAX_SEQ_LEN'))
+NUM_HEADS = int(os.getenv('NUM_HEADS'))
+
+embed_path = os.path.join(os.getcwd(), "utils", "embed.pt") 
+
 def load_embeds (path : str = embed_path ): 
     return torch.load(path,weights_only=False)
+
 
 
 class DepedencyEmbedding(nn.Module):
@@ -27,18 +42,40 @@ class DepedencyEmbedding(nn.Module):
 
 
 
+class SelfAttention(nn.Module):
+    def __init__(self, hid_dim : int, rotary_emb : torch):
+        super().__init__()
+        self.hid_dim = hid_dim
+
+    def forward(self, x : torch.Tensor, mask : torch.Tensor) -> torch.Tensor:
+        pass
+
+
+
+
+
 class DependencyEncoder(nn.Module):
     def __init__(self, num_features):
         super().__init__()
-        self.num_features = num_features
         self.dep_emb = DepedencyEmbedding(num_features)
         self.emb = load_embeds()
-        self.rotary_emb = torchtune.modules.RotaryPositionalEmbeddings(dim)
+        self.rotary_emb = RotaryPositionalEmbeddings(dim=num_features//NUM_HEADS, max_seq_len= MAX_SEQ_LEN, base= 10000)
     
     def forward (self, in_ids : torch.Tensor, mask : torch.Tensor) -> torch.Tensor:
-        assert in_ids.shape[0] == mask.shape[0], f"size mismatch \ninput_ids : {in_ids.shape} \nmask : {mask.shape}"
-        
+        assert in_ids.shape[-1] == mask.shape[-2] == MAX_SEQ_LEN, f"size mismatch \ninput_ids : {in_ids.shape} \nmask : {mask.shape}"
+
+        # tokens id to vector
+        # batch_size x seq_len x num_features
+        x = self.emb(in_ids)
+
+        # dependency mask to mask vector
+        # batch_sizer x seq_len x num_features
+        mask_vec = self.dep_emb(mask)
+
+
+        # padding the sequence 
         pass
+        
 
 
 
